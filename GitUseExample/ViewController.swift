f5177fc29c2e56bb35bae4db721f767290c59f75
saikatghosh <ghoshsaikat83@gmail.com>
2018-11-16 01:42:24 +0530

    Commit for Remote

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("This is a Git tutorial")
         
+        print("You finished!")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
